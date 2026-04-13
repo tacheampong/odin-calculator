d9d699d0f9bfe941f4f2b17e4f965f325b5c4d4a
tacheampong <th.acheampong@gmail.com>
2026-04-13 11:31:02 -0400

    create calculator function for add subtract multiply and divide

---
+++ script.js
@@ -0,0 +1,21 @@
+// Add
+function add(num1, num2){
+    return num1 + num2;
+}
+
+// Subtract
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+// Multiply
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+// Divide
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+// Operate
