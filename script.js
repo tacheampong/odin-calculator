b1dbbd7f0f70c6773cd1abedb72b95c6b78a489d
tacheampong <th.acheampong@gmail.com>
2026-04-16 12:42:36 -0400

    add a registry of operators and remove global variables

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 
-var operator = "";
-var n1 = 0;
-var n2 = 0;
-
 // Add
 function add(num1, num2){
     return num1 + num2;
@@ -22,8 +18,20 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 / num2;
 }
+export function registry(operator){
+    switch (operator){
+        case "+":
+            return add;
+        case "-":
+            return subtract;
+        case "*":
+            return multiply;
+        case "/":
+            return divide;    
+    }
+}
 
 // Operate
-function operate(operator, num1, num2){
+export function operate(operator, num1, num2){
     return operator(num1, num2)
 }
