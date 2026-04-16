c50c23831af3fc8d7fcf8bc78c7809af1d3ae090
tacheampong <th.acheampong@gmail.com>
2026-04-16 10:46:31 -0400

    remove inline style from script

--- script2.js
+++ script2.js
@@ -1,37 +1,19 @@
 function createCalculator() {
   var outerBox = document.createElement("div");
   outerBox.className = "calculator";
-  outerBox.style.width = "300px";
-  outerBox.style.height = "400px";
-  outerBox.style.border = "1px solid grey";
-  outerBox.style.boxShadow = "0 0 1px 0px";
-  outerBox.style.borderRadius = "5px";
-  outerBox.style.backgroundColor = "#f0f0f0";
   return outerBox;
 }
 function createScreen() {
   var screen = document.createElement("div");
   screen.className = "screen";
-  screen.style.width = "250px";
-  screen.style.height = "50px";
-  screen.style.border = "1px solid grey";
-  screen.style.backgroundColor = "white";
-  screen.innerText = "1000";
-  screen.style.fontSize = "30px";
-  screen.style.textAlign = "end";
-  screen.style.borderRadius = "2px";
   return screen;
 }
 function createButton() {
   var button = document.createElement("div");
   button.className = "button";
-  button.style.width = "50px";
-  button.style.height = "50px";
-  button.style.border = "1px solid grey";
-  button.style.borderRadius = "8px";
-  button.style.backgroundColor = "white";
-  button.style.textAlign = "center";
-  button.style.fontSize = "30px";
+  button.addEventListener("click", (event) => {
+     handleUpdates(screen, event)
+  })
   return button;
 }
 
@@ -39,9 +21,6 @@ function createDigits() {
   var container = document.createElement("div");
   var digits = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, "CL"];
   container.className = "digit-container";
-  container.style.display = "flex";
-  container.style.flexWrap = "wrap";
-  container.style.gap = "5px";
   digits.forEach((digit) => {
     var button = createButton();
     button.innerText = `${digit}`;
@@ -54,9 +33,6 @@ function createOperators() {
   var container = document.createElement("div");
   var operators = ["+", "-", "*", "/", "="];
   container.className = "operators-container";
-  container.style.display = "flex";
-  container.style.flexDirection = "column";
-  container.style.gap = "5px";
   operators.forEach((operator) => {
     var button = createButton();
     button.style.backgroundColor = "orange";
@@ -66,21 +42,25 @@ function createOperators() {
   });
   return container;
 }
+// when the event listeners is activated 
+// handle updates is called the screen updates with
+// the text of the current button
+function handleUpdates(screen,button){
+    screen.innerText = screen.innerText + button.innerText
+}
 //TODO: clean up/abstract the containers
 //TODO: move styling to css file
+//TODO: FIX so that screen is in the dom and the button can access it
 window.addEventListener("load", () => {
   var calculator = createCalculator();
   var screen = createScreen();
   var digits = createDigits();
   var operators = createOperators();
+
   var innerContainer = document.createElement("div");
   var buttonContainer = document.createElement("div");
-  buttonContainer.style.display = "flex";
-  buttonContainer.style.alignItems = "flex-start";
-  innerContainer.style.display = "flex";
-  innerContainer.style.flexDirection = "column";
-  innerContainer.style.gap = "10px";
-  innerContainer.style.margin = "25px";
+  buttonContainer.className = "button-container"
+  innerContainer.className = "inner-container"
   buttonContainer.append(digits);
   buttonContainer.append(operators);
   innerContainer.append(screen);
@@ -88,4 +68,6 @@ window.addEventListener("load", () => {
 
   calculator.append(innerContainer);
   document.body.append(calculator);
+
+
 });
