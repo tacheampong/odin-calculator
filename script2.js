db4a564fef3b0ca44168ac9608f766aef4b80a82
tacheampong <th.acheampong@gmail.com>
2026-04-16 12:45:02 -0400

    add ability to operate on two values using regex and view results in the console

--- script2.js
+++ script2.js
@@ -1,3 +1,4 @@
+import {registry, operate } from "./script.js";
 function createCalculator() {
   var digits = createDigits();
   var operators = createOperators();
@@ -14,7 +15,14 @@ function createButton() {
   var screen = document.querySelector(".screen")
   button.className = "button";
   button.addEventListener("click", (event) => {
-     handleUpdates(screen, event.target)
+    if (event.target.innerText == "CL"){
+        screen.innerHTML = "";
+        return button
+    } 
+    else {
+        handleUpdates(screen, event.target)
+    }
+     
   })
   return button;
 }
@@ -31,15 +39,31 @@ function createDigits() {
   });
   return container;
 }
+function assign(button, screen){
+    var currentText = screen.innerText
+    var op = button.innerText
+    var match = currentText.match(/^(\d+)\s*([+\-*/])\s*(\d+)\s*=?$/)
+    if (match) {
+        //skip full match
+        const [, num1, operator, num2] = match;
+        console.log(operate(registry(operator),Number(num1), Number(num2)))
+        
+    }
+    
+}
 function createOperators() {
   var container = document.createElement("div");
   var operators = ["+", "-", "*", "/", "="];
+  var screen = document.querySelector(".screen")
   container.className = "operators-container";
   operators.forEach((operator) => {
     var button = createButton();
     button.style.backgroundColor = "orange";
     button.style.fontWeight = "bold";
     button.innerText = `${operator}`;
+    button.addEventListener("click", (event) =>{
+        assign(event.target,screen)
+    })
     container.append(button);
   });
   return container;
